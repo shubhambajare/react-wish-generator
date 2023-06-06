b32f487537c9395e7d7d74bd01e258c25260beb1
shubhambajare <shubhambajare8@gmail.com>
2023-06-06 23:33:58 +0530

    CSS

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 
 const MessageCard = (props) => {
     return (
-        <div className='m-5 ws-display-inline-block'>
-            <Card body className='p-4 ws-message-card'>{props.message}</Card>
+        <div className='m-5 ws-display-inline-block ws-message-card-wrapper'>
+
+            <Card body className='p-4 mt-3 ws-message-card'>{props.message}</Card>
+            <div className="mt-3">
+                <Button variant="outline-success" style={{ marginRight: '10px' }} onClick={props.changeMessage}>
+                    <span >
+                        Change Message
+                    </span>
+                </Button>
+                <Button variant="outline-warning" onClick={() => props.generateNewMessage(false)}>
+                    <span  >
+                        Generate New Message
+                    </span>
+                </Button>
+            </div>
+
         </div>
     )
 }
