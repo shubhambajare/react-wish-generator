995d1c19523c31fdd0a5f339e8bd3ba408e153f6
shubhambajare <shubhambajare8@gmail.com>
2023-06-10 20:09:01 +0530

    download image functionality added

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { exportComponentAsJPEG } from 'react-component-export-image';
 
 const MessageCard = (props) => {
     const [copyBtnText, setCopyBtnText] = useState('Copy Message Link');
+    const messageCardRef = useRef();
     return (
         <div className='m-4 ws-display-inline-block ws-message-card-wrapper'>
 
-            <Card body className='p-4 mt-3 ws-message-card'>{props.message}</Card>
+            <Card ref={messageCardRef} body className='p-4 mt-3 ws-message-card'>{props.message}</Card>
             <div className="mt-3">
                 <Button variant="outline-success" style={{ marginRight: '10px', marginBottom: '15px' }} onClick={props.changeMessage}>
                     <span >
@@ -21,6 +23,12 @@ const MessageCard = (props) => {
                         {copyBtnText}
                     </Button>
                 </CopyToClipboard>
+
+                <Button variant="outline-success" style={{ marginRight: '10px', marginBottom: '15px' }} onClick={() => exportComponentAsJPEG(messageCardRef)}>
+                    <span >
+                        Download Image
+                    </span>
+                </Button>
 
                 <div>
                     <Button variant="outline-warning" style={{ marginRight: '10px', marginBottom: '15px' }} onClick={() => props.generateNewMessage(false)}>
